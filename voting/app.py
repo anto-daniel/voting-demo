2cc638cd5e7d1c9925508cf971015a7acf0bcadf
root <root@ip-172-31-48-191.ec2.internal>
2018-01-21 10:26:48 +0000

    change

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Anil"
+optionB = "Super"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
